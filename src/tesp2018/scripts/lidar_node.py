2f27dac013a1b862b3dfe577efc2f180963f2492
Brudasoe <serafadam@gmail.com>
2018-08-09 18:38:10 +0900

    lidar node update

--- src/tesp2018/scripts/lidar_node.py
+++ src/tesp2018/scripts/lidar_node.py
@@ -2,9 +2,11 @@
 import rospy
 import numpy
 from sensor_msgs.msg import LaserScan
-from rospy.numpy_msg import numpy_msg
-from rospy_tutorials.msg import Floats
-from std_msgs.msg import Float64MultiArray
+from pythonosc import udp_client
+
+pd_ip = "172.16.20.57"
+pd_port = 11112
+client = udp_client.SimpleUDPClient(pd_ip, pd_port)
 
 
 class LidarNode(object):
@@ -14,18 +16,36 @@ class LidarNode(object):
         self.lidar_subscriber = rospy.Subscriber('/scan', LaserScan, self.lidar_callback)
         self.data_counter = 0
         self.start_time = 0
-        self.pub = rospy.Publisher('/lidar_output', numpy_msg(Floats),queue_size=10)
+        #self.pub = rospy.Publisher('/lidar_output', numpy_msg(Floats),queue_size=10)
     def lidar_callback(self, data):
         #print len(data.ranges)
         laser = [data.ranges[0],data.ranges[45], data.ranges[90],data.ranges[135], data.ranges[179], data.ranges[-45], data.ranges[-90], data.ranges[-135]]
-        #print type(laser)
 
-        laser_ros = numpy.array(laser)
-        laser_ros.layout.dim=[8]
-        laser_ros.data = laser
-        print laser_ros
-        self.pub.publish(laser_ros)
-        #self.data_saving(self.laser)
+        self.shake_it_baby(laser)
+
+
+    def shake_it_baby(self, lasers):
+
+        if lasers[6] > 0:
+            right_amp = 0.1 / abs(lasers[6])
+            left_amp = 0.001
+        if lasers[2] > 0:
+            right_amp  = 0.001
+            left_amp = 0.1 / abs(lasers[2])
+
+        if lasers[0] >= 0:
+            front_amp =0.1 / abs(lasers[0])
+            back_amp = 0.001
+        if lasers[4]:
+            front_amp = 0.001
+            back_amp = 0.1 / abs(lasers[4])
+
+        client.send_message("/front", front_amp)
+        client.send_message("/back", back_amp)
+        client.send_message("/left", left_amp)
+        client.send_message("/right", right_amp)
+        print(front_amp, back_amp, left_amp, right_amp)
+
 
     def data_saving(self,data):
         if self.data_counter == 0:
